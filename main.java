99fb06a534d50c7e8af7ad7554e7129a7eb3c8d0
Target <142077321+Target-blackbox@users.noreply.github.com>
2025-11-13 05:04:30 +0000

    java_basics

---
+++ main.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args){
+        // System.out.println("hello,java");
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter a number:");
+        // int num = sc.nextInt();
+        // System.out.println(num);
+        // if (num % 2 == 0)
+        // System.out.println("its an even number");
+        // else
+        // System.out.println("its an odd number");
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("enter a number:");
+        // int a = sc.nextInt();
+        // int sum = 0;
+        // for (int i=1;i<=a;i++){
+        //     sum = sum + i;
+        // }
+        // System.out.println(sum);
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("enter a number:");
+        // int a = sc.nextInt();
+        // int fact = 1;
+        // for ( int i = 1; i<=a; i++){
+        //     fact = fact * i;
+        // }
+        // System.out.println(fact);
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("enter a number:");
+        // int a = sc.nextInt();
+        // int temp = a;
+        // int rev = 0;
+        // while (a != 0){
+        //     int digit = a % 10;
+        //     rev = rev*10 + digit;
+        //     a = a/10;
+        // }
+        // System.out.println(rev);
+
+        // if (rev == temp){
+        //     System.out.print("yes");
+        // }
+        // else{
+        //     System.out.print("no");
+        // }
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("enter a number:");
+        // int a = sc.nextInt();
+        // int n1 = 0;
+        // int n2 = 1;
+        // int n3;
+        // for (int i = 2;i<10;i++){
+        //     n3 = n1+n2;
+        //     System.out.println(" "+n3);
+        //     n1 = n2;
+        //     n2 = n3;
+        // }
+
+
+        
+    }
+
+}
